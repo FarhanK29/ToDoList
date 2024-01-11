14d2831eabca89ae8c2e8c8e02769378a93575bb
Farhan Khan <farhan.k0429@gmail.com>
2024-01-11 11:59:25 -0500

    All html, css, and javascript set to allow full functionality of website

---
+++ script.js
@@ -0,0 +1,80 @@
+const addBtn = document.querySelector("#addBtn");
+const addInput = document.querySelector("#input-box");
+const divList = document.querySelector(".list-container");
+
+addInput.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        if (addInput.value === "") {
+            alert("You must type something");
+        } else {
+            addBtn.click();
+        }
+    }
+    saveData();
+});
+
+addBtn.addEventListener("click", () => {
+    if (addInput.value === "") {
+        alert("You must type something");
+    } else {
+        const ul = divList.querySelector("ul");
+        const li = document.createElement("li");
+        li.innerHTML = addInput.value;
+        ul.appendChild(li);
+        addInput.value = "";
+        let span = document.createElement("span");
+        span.innerHTML =
+            '<button class="check">&#10003;</button><button class="cross">&#10005;</button>';
+        li.appendChild(span);
+        saveData();
+
+
+        const crossButton = li.querySelector(".cross");
+        crossButton.addEventListener("click", function () {
+            this.closest("li").remove();
+            saveData();
+        });
+        const checkButton = li.querySelector(".check");
+        checkButton.addEventListener("click", function () {
+            li.classList.toggle("strikethrough");
+            saveData();
+        });
+
+    
+    }
+});
+
+function saveData(){
+    localStorage.setItem("data", divList.innerHTML);
+}
+
+function showTask() {
+    divList.innerHTML = localStorage.getItem("data");
+
+    // Restore event listeners after setting innerHTML
+    const crossButtons = document.querySelectorAll(".cross");
+    crossButtons.forEach(button => {
+        button.addEventListener("click", function () {
+            this.closest("li").remove();
+            saveData();
+        });
+    });
+
+    const checkButtons = document.querySelectorAll(".check");
+    checkButtons.forEach(button => {
+        button.addEventListener("click", function () {
+            const li = this.closest("li");
+            li.classList.toggle("strikethrough");
+            saveData();
+        });
+    });
+}
+
+// Call showTask after setting up event listeners
+showTask();
+
+// function showTask(){
+//     divList.innerHTML = localStorage.getItem("data");
+// }
+
+// showTask();
